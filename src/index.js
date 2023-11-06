900958b444f34f3681bfb0445d8583879513905c
Sabircan <Sabircan@gmail.com>
2023-11-06 12:45:41 +0100

    removed old files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import "./style.scss";
 import "./scripts/localstorage";
-import "./scripts/artikel";
-import "./scripts/karussel";
-import "./scripts/progress";
-import "./scripts/todo";
 import "./scripts/darkmode";
 import "./scripts/touch";
